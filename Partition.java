042f38924a69293665bb50fd02ff026818a694e7
Sophie Nichol <snichol00@stuy.edu>
2019-03-09 20:30:38 -0500

    partition works mostly

--- Partition.java
+++ Partition.java
@@ -1,5 +1,9 @@
-public class Partition{}
-  int partition ( int [] data, int start, int end){
+import java.util.Arrays;
+import java.util.Random;
+import java.lang.Math;
+
+public class Partition{
+  public static int partition(int [] data, int start, int end){
     //choose random pivot
     Random r = new Random();
     int pIdx = r.nextInt(data.length);
@@ -11,7 +15,7 @@ public class Partition{}
 
     //once start and end are equal, array has been partitioned
     while (start != end){
-      if (data[start] < pivot){
+      if (data[start] > pVal){
         //swap start and end
         int temp = data[start];
         data[start] = data[end];
@@ -22,4 +26,16 @@ public class Partition{}
         start++;
       }
     }
+    //once sorted, you need to switch the pivot back into place
+    if (data[start] > pVal){
+      data[0] = data[start - 1];
+      data[start - 1] = pVal;
+      return start - 1;
+    }
+    else{
+      data[0] = data[start];
+      data[start] = pVal;
+      return start;
+    }
   }
+}
