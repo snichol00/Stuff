85027437a8be4dcba885cbe33214cfce90aba3e4
Sophie Nichol <snichol00@stuy.edu>
2019-03-11 22:21:50 -0400

    Partition works

--- Partition.java
+++ Partition.java
@@ -10,9 +10,9 @@ public class Partition{
     int pVal = data[pIdx];
 
     //swap pivot to be first int
-    data[pIdx] = data[0];
-    data[0] = pVal;
-
+    data[pIdx] = data[start];
+    data[start] = pVal;
+    //System.out.println(Arrays.toString(data));
     //once start and end are equal, array has been partitioned
     while (start != end){
       if (data[start] > pVal){
@@ -26,6 +26,8 @@ public class Partition{
         start++;
       }
     }
+    //System.out.println("loop finished");
+    //System.out.println(Arrays.toString(data));
     //once sorted, you need to switch the pivot back into place
     if (data[start] > pVal){
       data[0] = data[start - 1];
@@ -38,4 +40,13 @@ public class Partition{
       return start;
     }
   }
+
+  public static void main(String[] args) {
+    int[] data = {1,7,4,2,3,6,5,4,2,7,5};
+    //for (int i=0;i<10;i++) {
+      partition(data,0,10);
+
+    //}
+  }
+
 }
